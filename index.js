9b006091bc681358b7e0d07cff226b673f1327f1
Theo DUPIN <Theo.DUPIN@etu.uca.fr>
2023-10-29 13:02:17 +0100

    :sparkles: add: Swagger

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const swagger = require('./swagger');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) =>{
     res.send('Answerz API');
 });
+
+app.use('/api/v1', swagger.serve, swagger.setup);
 
 app.listen(port, () => {
     console.log(`Server Express listening on port ${port}`);
---
+++ swagger.js
@@ -0,0 +1,22 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Answerz API',
+            version: '1.0.0',
+            description: 'Documentation for Answerz API',
+        },
+        basePath: '/',
+    },
+    apis: [__dirname + '/index.js'],
+};
+
+const specs = swaggerJsDoc(options);
+
+module.exports = {
+    serve: swaggerUi.serve,
+    setup: swaggerUi.setup(specs),
+};

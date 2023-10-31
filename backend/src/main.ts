866c32a5f6c9e8bc28a8c8a82856465b5bfe614a
Theo DUPIN <Theo.DUPIN@etu.uca.fr>
2023-11-01 00:29:34 +0100

    :hammer: add: new route for Swagger API(/api-docs)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .addTag('users')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(3000);
 }
 bootstrap();

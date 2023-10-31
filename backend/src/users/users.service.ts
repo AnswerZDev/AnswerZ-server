b89b31539e2f5c37e08e482494ec6077e0c2a92c
Theo DUPIN <Theo.DUPIN@etu.uca.fr>
2023-10-31 23:38:35 +0100

    :bricks: add: Nestjs framework REST API

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { User } from 'src/interfaces/user.interface';
+
+@Injectable()
+export class UsersService {
+
+    private readonly users: User[] = [
+        {
+            firstname: 'Alexis',
+            lastname: 'Carreau',
+            email: 'alexis.carreau@gmail.com',
+            password: 'alexis123',
+        },
+        {
+            firstname: 'Théo',
+            lastname: 'Dupin',
+            email: 'theo.dupin@gmail.com',
+            password: 'theo123',
+        }
+    ];
+
+    create(user: User) {
+        this.users.push(user);
+    }
+
+    findAll(): User[] {
+        return this.users;
+    }
+
+}

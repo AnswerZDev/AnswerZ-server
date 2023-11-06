ec9d33aab8a377d3b972bcce2350161e4cd5118b
Alexis1663 <92861778+Alexis1663@users.noreply.github.com>
2023-11-07 00:01:23 +0100

    :hammer: build: login authentification with jwt token

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,14 +1,35 @@
-import { Injectable } from '@nestjs/common';
-import { SignupUserDto } from '../dto/signup-user.dto';
-import { LoginUserDto } from '../dto/login-user.dto';
+import { Injectable } from "@nestjs/common";
+import { SignupUserDto } from "../dto/signup-user.dto";
+import { LoginUserDto } from "../dto/login-user.dto";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getIdToken } from "@firebase/auth";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class AuthService {
+  constructor(private configService: ConfigService) {}
+
+  private _firebaseConfig = {
+    apiKey: process.env.API_KEY,
+  };
   signup(signUpDto: SignupUserDto) {
     return signUpDto;
   }
 
-  login(loginDto: LoginUserDto) {
-    return loginDto;
+  async login(loginDto: LoginUserDto) {
+    const { email, password } = loginDto;
+    const app = initializeApp(this._firebaseConfig);
+    const auth = getAuth(app);
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password,
+    );
+    // Signed in
+    const user = userCredential.user;
+    // Génére un token JWT pour l'utilisateur et le retourne au format JSON
+    const token = await getIdToken(user);
+    return { token };
   }
 }

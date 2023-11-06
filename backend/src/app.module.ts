84f5546af79df6aa6adbd7870d3833ae110e0528
Alexis1663 <92861778+Alexis1663@users.noreply.github.com>
2023-11-07 00:01:23 +0100

    :hammer: build: login authentification with jwt token

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersModule } from './users/users.module';
-import { AuthController } from './auth/auth.controller';
-import { AuthModule } from './auth/auth.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersController } from "./users/users.controller";
+import { UsersModule } from "./users/users.module";
+import { AuthController } from "./auth/auth.controller";
+import { AuthModule } from "./auth/auth.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController, UsersController, AuthController],
   providers: [AppService],
 })

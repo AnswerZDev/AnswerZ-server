d9ee549dc278bc656b03eb90f935c233c9175b44
Alexis1663 <92861778+Alexis1663@users.noreply.github.com>
2023-11-25 21:24:37 +0100

    :card_file_box: add: postgrey database + pgAdmin

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,27 @@ import { UsersModule } from "./users/users.module";
 import { AuthController } from "./auth/auth.controller";
 import { AuthModule } from "./auth/auth.module";
 import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [UsersModule, AuthModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as unknown as any,
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      retryAttempts: parseInt(process.env.DATABASE_RETRY_ATTEMPTS),
+    }),
+  ],
   controllers: [AppController, UsersController, AuthController],
   providers: [AppService],
 })

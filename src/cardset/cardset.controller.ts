915ef31f6f6ac08966881f5a0c5c33516edeaa57
Destroyeur6315 <romain.fillot630@gmail.com>
2024-01-28 00:16:48 +0100

    :sparkles: new route GET for cardset

--- src/cardset/cardset.controller.ts
+++ src/cardset/cardset.controller.ts
@@ -1,8 +1,21 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, HttpException, HttpStatus } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
+import { CardsetService } from "./cardset.service";
 
 @ApiTags("Cardset")
 @Controller("Cardset")
 export class CardsetController {
-  constructor() {}
+  constructor(private cardset_service: CardsetService) {}
+
+  @Get()
+  async getAll() {
+    try {
+      const datas = await this.cardset_service.getAllCardset();
+      return datas;
+    } catch (error) {
+      throw new HttpException('Error data not found', HttpStatus.NOT_FOUND);
+    }
+  }
+
+
 }
--- src/cardset/cardset.service.ts
+++ src/cardset/cardset.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Cardset } from "src/entities/Cardset.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
-export class CardsetService {}
+export class CardsetService {
+
+    constructor(
+        @InjectRepository(Cardset)
+        private carSetRepository: Repository<Cardset>
+    ){
+
+    }
+
+    async getAllCardset() : Promise<Cardset[]> {
+        return await this.carSetRepository.find();
+    }
+
+}

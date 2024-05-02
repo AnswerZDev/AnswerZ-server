bf26e4f4c113fbdf326adac82b85695dcf3afa6e
Alexis1663 <92861778+Alexis1663@users.noreply.github.com>
2024-05-02 20:38:47 +0200

    :construction: wip: upload file

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import {Module} from "@nestjs/common";
 import {FirebaseService} from "./services/firebase.service";
+import {FileUploadService} from "./services/file-upload.service";
 
 
 @Module({
-  providers: [FirebaseService],
-  exports: [FirebaseService],
+    providers: [
+        FirebaseService,
+        FileUploadService
+    ],
+    exports: [
+        FirebaseService,
+        FileUploadService
+    ],
 })
-export class SharedModule{
+export class SharedModule {
 }

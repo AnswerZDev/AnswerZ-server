271c232f57ac0044068e8508943e4e687ec3c90e
Audric-S <sabatieraudric@gmail.com>
2024-05-14 11:32:10 +0200

    :building_construction: add(QuizGame): create game class to store game settings (link to rooms)

--- quiz_game_part/src/main.ts
+++ quiz_game_part/src/main.ts
@@ -1,11 +1,19 @@
-// main.ts
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Activer CORS pour l'application NestJS
   app.enableCors();
+
+  // Configurer CORS spécifiquement pour Socket.IO
+  app.use(cors({
+    origin:  "http://localhost:4200", // Remplacez cela par l'URL de votre front-end Angular
+    credentials: true,
+  }));
+
   await app.listen(3000);
 }
 bootstrap();

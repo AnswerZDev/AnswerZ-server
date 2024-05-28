5e16f9cd67dc7252fdd98f8384a402f2f579161d
Audric-S <sabatieraudric@gmail.com>
2024-05-28 10:30:58 +0200

    :sparkles: add(QuizGame): leave functionality (without page refresh)

--- quiz_game_part/debug/rooms.debug.ts
+++ quiz_game_part/debug/rooms.debug.ts
@@ -4,6 +4,8 @@ import { RoomService } from '../src/rooms/room.service';
 export class RoomDebug {
 
   static displayActualRoomStates(server: Server) {
+    const roomService = new RoomService();
+
     const availableRooms = RoomService.getAvailableRooms(server);
     console.log(availableRooms);
   }

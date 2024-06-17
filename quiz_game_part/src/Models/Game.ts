6bcd55595bde55d33431c83c07e61ba46326a836
Audric-S <sabatieraudric@gmail.com>
2024-06-17 15:10:44 +0200

    :sparkles: add answer's binfing in game components

--- quiz_game_part/src/Models/Game.ts
+++ quiz_game_part/src/Models/Game.ts
@@ -1,3 +1,4 @@
+import { Question } from "./Question";
 
 export class Game {
     private gameId: string;
@@ -7,7 +8,10 @@ export class Game {
     host: string;
     actualQuestionIndex : number = 0;
     isLaunch: boolean = false;
-    questions = ['EN quelle année à eu lieu la 1ere Guerre mondiale ?', 'testQUestion2'];
+    questions: Question[] = [
+        { question: 'En quelle année a eu lieu la 1ère Guerre mondiale ?', answers: ['39-45', '13-16', '14-18', 'test'] },
+        { question: 'testQuestion2',answers: ['39-43', 'test', '14-18', 'test'] }
+    ];
     private description = "Accenderat super his incitatum propositum ad nocendum aliqua mulier vilis, quae ad palatium ut poposcerat intromissa insidias ei latenter obtendi prodiderat a militibus obscurissimis. quam Constantina exultans ut in tuto iam locata mariti salute muneratam vehiculoque inpositam per regiae ianuas emisit in publicum, ut his inlecebris alios quoque ad indicanda proliceret paria vel maiora.";
     private image = "assets/images/game-picture.jpeg";
     private title = "John doe's game";

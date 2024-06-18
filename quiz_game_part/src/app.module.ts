20d2d9aedc2b2d9580c49499ceae7e148ebce166
Alexis1663 <92861778+Alexis1663@users.noreply.github.com>
2024-06-18 16:18:41 +0200

    :bug: fix: database uses

--- quiz_game_part/src/app.module.ts
+++ quiz_game_part/src/app.module.ts
@@ -1,31 +1,32 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { SocketModule } from './socket/socket.module';
-import { QuizModule } from './quiz/quiz.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Quiz } from './entities/quiz.entity';
-import { ConfigModule } from '@nestjs/config';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {SocketModule} from './socket/socket.module';
+import {QuizModule} from './quiz/quiz.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {ConfigModule} from '@nestjs/config';
 
 @Module({
-  imports: [
-    SocketModule,
-    QuizModule,
-    TypeOrmModule.forRoot({
-        type: process.env.DATABASE_TYPE as unknown as any,
-        host: process.env.DATABASE_HOST,
-        port: parseInt(process.env.DATABASE_PORT),
-        username: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-        entities: [Quiz],
-        synchronize: true,
-    }),
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        SocketModule,
+        QuizModule,
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        TypeOrmModule.forRoot({
+            type: process.env.DATABASE_TYPE as unknown as any,
+            host: process.env.DATABASE_HOST,
+            port: parseInt(process.env.DATABASE_PORT),
+            username: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
+            autoLoadEntities: true,
+            synchronize: true,
+            retryAttempts: parseInt(process.env.DATABASE_RETRY_ATTEMPTS),
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
--- quiz_game_part/src/main.ts
+++ quiz_game_part/src/main.ts
@@ -1,40 +1,41 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 import * as cors from 'cors';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  // Activer CORS pour l'application NestJS
-  app.enableCors();
+    // Activer CORS pour l'application NestJS
+    app.enableCors();
 
-  const config = new DocumentBuilder()
-    .setTitle("Answerz API")
-    .setDescription("The AnswerZ API description")
-    .setVersion("1.0")
-    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT', in: 'header' },
-      'access-token', // C'est ici que vous définissez le nom du Bearer Token
-    )
-    .build();
+    const config = new DocumentBuilder()
+        .setTitle("Answerz API")
+        .setDescription("The AnswerZ API description")
+        .setVersion("1.0")
+        .addBearerAuth({type: 'http', scheme: 'bearer', bearerFormat: 'JWT', in: 'header'},
+            'access-token', // C'est ici que vous définissez le nom du Bearer Token
+        )
+        .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api-docs", app, document, {
-    swaggerOptions: {
-        persistAuthorization: true,
-        tagsSorter: "alpha",
-    },
-  });
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api-docs", app, document, {
+        swaggerOptions: {
+            persistAuthorization: true,
+            tagsSorter: "alpha",
+        },
+    });
 
-  // Configurer CORS spécifiquement pour Socket.IO
-  app.use(cors({
-    origin:  "http://localhost:4200",
-    credentials: true,
-  }));
+    // Configurer CORS spécifiquement pour Socket.IO
+    app.use(cors({
+        origin: "http://localhost:4200",
+        credentials: true,
+    }));
 
-  app.use(express.static('public'));
+    app.use(express.static('public'));
 
-  await app.listen(3100);
+    await app.listen(3100);
 }
+
 bootstrap();
